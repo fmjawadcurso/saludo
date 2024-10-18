ebc82c138fc6098e21d48c9a55a9ff7585776c77
fmjawadcurso <172632482+fmjawadcurso@users.noreply.github.com>
2024-10-18 15:26:40 +0200

    cambiar el texto de la msn

--- src/saludo.php
+++ src/saludo.php
@@ -2,13 +2,15 @@
 namespace Saludo\Saludo;
 class Saludo {
     private $msn;
+    private $usuario;
 
-    public function __construct(){
+    public function __construct($usuario){
         $this->setCrearSaludo();
+        $this->usuario = $usuario;
     }
 
     private function  setCrearSaludo(){
-       $this->msn = "HOLA MUNDO";
+       $this->msn = "bienvenido,Sr/Sra".$this->usuario;
     }
 
     public function getCrearSaludo(){
